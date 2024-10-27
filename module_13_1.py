aad64096b1bce726a7865b7d180f1c7c669c822c
nikitinale7na <nikitina.gby@gmail.com>
2024-10-27 13:01:03 +0300

    Add files via upload

---
+++ module_13_1.py
@@ -0,0 +1,25 @@
+import time
+import asyncio
+
+
+async def start_strongman(name, power):
+    print(f'Силач {name} начал соревнования.')
+    for i in range(1, 6):
+        await asyncio.sleep(5/power)
+        print(f'Силач {name} поднял шар {i}')
+    print(f'Силач {name} закончил соревнования')
+
+
+async def start_tournament():
+    task1 = asyncio.create_task(start_strongman('Pasha', 3))
+    task2 = asyncio.create_task(start_strongman('Denis', 4))
+    task3 = asyncio.create_task(start_strongman('Apollon', 5))
+    await task1
+    await task2
+    await task3
+
+
+start = time.time()  # время начала соревнований
+asyncio.run(start_tournament())
+end = time.time()  # время конца соревнований
+print(f'Соревнования длились {round(end - start, 2)} секунд')
