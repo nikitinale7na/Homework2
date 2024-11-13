f54600b06799c20ebf61f1233213fc6d826cdfb8
nikitinale7na <nikitina.gby@gmail.com>
2024-11-13 15:08:27 +0300

    Add files via upload

---
+++ modul_13_3_1.py
@@ -0,0 +1,23 @@
+#  "Методы отправки сообщений". Задача "Он мне ответил!"
+from aiogram import Bot, Dispatcher,executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from config import TOKEN
+
+import asyncio
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'])
+async def start_message(message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+@dp.message_handler()
+async def all_message(message):
+    print('Введите команду /start, чтобы начать общение.')
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
